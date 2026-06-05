525318be118798a0df642e1e850b29163ae08d48
Simon Eklundh <github@oakgrove.site>
2026-06-05 11:25:39 +0200

    rewrite to shorter season and episode naming scheme

--- jellyfin-youtube-downloader.py
+++ jellyfin-youtube-downloader.py
@@ -69,9 +69,9 @@ def ensure_channel_images(youtube_id, series_name, season_name):
 
 def download_video(youtube_id, series_name, season_name):
     url = f"https://www.youtube.com/watch?v={youtube_id}"
-    season_number = season_name.split()[-1]
+    season_number = season_name.split()[-1][-2:]
     opts = {
-        "outtmpl": f"/downloads/{series_name}/{season_name}/%(title)s - S{season_number}E%(upload_date>%m%d)s.%(ext)s",
+        "outtmpl": f"/downloads/{series_name}/{season_number}/%(title)s - S{season_number}E%(upload_date>%j)s.%(ext)s",
         "embedthumbnail": True,
         "embedmetadata": True,
         "writesubtitles": True,
